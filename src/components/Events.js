c8d3a1707eaf0b34a74460f515382a290974ff65
RutendoN <RutendoN@Saratoga.local>
2020-03-04 16:03:09 +0200

    Card 80% done

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,5 +1,10 @@
 import React, { useState, useEffect } from "react";
 import Api from "../api/Api";
+import "../styles/eventCard.css";
+import Menu from "./Menu";
+import { Route } from "react-router-dom";
+
+//import "../styles/login.css";
 
 export default function Events(){
 
@@ -8,20 +13,38 @@ export default function Events(){
     useEffect(() => {
       Api.getRequest("events")
         .then(response => response.json())
-        .then(data => setData(data));
+        .then(data => {setData(data); console.log(data)});
     },[]);
 
     return (
+        
         <div>
-
-            {data.map( x =>(
+            <Route exact path="/" render={() => <Menu />} />
+            <button id="CreateEvent" class="button">CreateEvent</button>
+           <div>
+            {data.reverse().map( x =>(
                 <div>
                     
-                    <h1>{x.name} </h1>
-                    <h2>price: R {x.ticketPrice}</h2>
-            
-                </div>
-            ))}
+                    <div className="container"> 
+                    <div className="card">
+                        <div class="card-body">
+                            <div className="card-top">
+                                <span className="card-lable">Event Name</span>
+                            </div>
+                            <span className="card-header">{x.name}</span>
+                            <div className="card-sub-botton">
+                            <span className="card-sub-text card-lable">Tickets {x.from} - {x.to}, Status: {x.active.toString()}</span>
+                            <span className="card-sub-text card-lable u-float-right">Price :  R {x.ticketPrice}</span>
+                            </div>
+                            <div className="card-sub-botton card-sub-show">
+                                <button  className="button card-sub-text card-link u-float-right">Allocate Tickets</button>
+                                <button  className="button card-sub-text card-link u-float-right">Return Tickets</button>
+                            </div>
+                            </div>
+                            </div>
+                            </div>
+                            </div>
+            ))}</div>
 
 
         </div>
