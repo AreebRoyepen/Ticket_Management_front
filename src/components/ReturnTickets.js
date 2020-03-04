4e8243261e4c25f5b1c193e84fa39590eefec90a
Areeb Royeppen <AreebR@Saratoga.local>
2020-03-04 15:39:03 +0200

    material ui components

--- src/components/ReturnTickets.js
+++ src/components/ReturnTickets.js
@@ -1,4 +1,8 @@
 import React, { useState, useEffect, useRef, useCallback } from "react";
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
 import Api from "../api/Api";
 import "../styles/login.css";
 
@@ -7,11 +11,15 @@ export default function ReturnTickets() {
 
     const [data, setData] = useState([]);
     
+    const [open, setOpen] = React.useState(false);
+    const [options, setOptions] = React.useState([]);
+    const loading = open && options.length === 0;
+
     const [isSending, setIsSending] = useState(false)
     const isMounted = useRef(true)
 
     const [ticketNumber, setTicketNumber] = useState(0);
-    const [person, setPerson] = useState([]);
+    const [person, setPerson] = useState(null);
     const [name, setName] = useState("");
 
     const [eventID, setEventID] = useState(0);
@@ -19,6 +27,36 @@ export default function ReturnTickets() {
     useEffect(() => {
      return isMounted.current = false
     },[]);
+
+
+    useEffect(() => {
+      let active = true;
+  
+      if (!loading) {
+        return undefined;
+      }
+  
+      (async () => {
+   
+      const response = await Api.getRequest("person");
+      const persons = await response.json();
+
+        if (active) {
+          setOptions(persons);;
+        }
+      })();
+  
+      return () => {
+        active = false;
+      };
+    }, [loading]);
+
+
+    useEffect(() => {
+      if (!open) {
+        setOptions([]);
+      }
+    }, [open]);
 
     const returnTicket = useCallback(async () => {
       // don't send again while we are sending
@@ -31,8 +69,8 @@ export default function ReturnTickets() {
 
       var x = {
         "ticketNumber": parseInt(ticketNumber),
-        "person": eventID,
-        "event" : person.ID
+        "person": person.id,
+        "event" : eventID
       };
 
       Api.postRequest("returnTicket",x)
@@ -46,30 +84,6 @@ export default function ReturnTickets() {
 
     }, [isSending, ticketNumber, eventID, person]); // update the callback if the state changes
 
-    const findPerson = useCallback(async () => {
-      // don't send again while we are sending
-      if (isSending) return
-
-      // update state
-      setIsSending(true)
-
-      // send the actual request
-
-      var x = {
-        "name" : name
-      };
-
-      Api.postRequest("personLikeName",x)
-      .then(response => response.json())
-      .then(data => {setPerson(data)});
-
-
-      // once the request is sent, update state again
-      if (isMounted.current) // only update if we are still mounted
-        setIsSending(false)
-
-    }, [isSending, name]); // update the callback if the state changes
-
     
     return (
 
@@ -77,20 +91,50 @@ export default function ReturnTickets() {
         <aside className="profile-card">
           <div className="profile-bio">
 
-      <input
-        type="text"
-        className="sign-up-input"
-        placeholder="Person name"
-        onChange={ e => setName(e.target.value)}
-      />
-      <input
-        type="text"
-        className="sign-up-input"
-        placeholder="ticket start number"
-        onChange={ e => setTicketNumber(e.target.value)}
+      
+          <Autocomplete
+          id="asynchronous-demo"
+          style={{ width: 250 }}
+          open={open}
+          onOpen={() => { setOpen(true); }}
+          onClose={() => {setOpen(false); }}
+          getOptionSelected={(option, value) => option.name === value.name}
+          getOptionLabel={option => option.name +" " +option.surname }
+          options={options}
+          loading={loading}
+          value = {person}
+          onChange={(event, newValue) => { setPerson(newValue); }}
+          renderInput={params => (
+            <TextField
+              {...params}
+              label="Select Person"
+              variant="outlined"
+              InputProps={{
+                ...params.InputProps,
+                endAdornment: (
+                  <React.Fragment>
+                    {loading ? <CircularProgress color="inherit" size={20} /> : null}
+                    {params.InputProps.endAdornment}
+                  </React.Fragment>
+                ),
+              }}
+          />
+        )}
       />
 
-      <button className = "button" type="button" disabled={isSending} onClick={returnTicket}> Allocate Ticket</button>
+      <TextField
+        id="filled-number"
+        label="ticket range from"
+        type="number"
+        onChange = {e => {setTicketNumber(e.target.value)}}
+        InputLabelProps={{
+          shrink: true,
+        }}
+        variant="outlined"
+        
+      />
+
+      <button className = "button" type="button" disabled={isSending} onClick={returnTicket}> Return Ticket</button>
   
       </div>
       </aside>
--- src/components/TicketAllocation.js
+++ src/components/TicketAllocation.js
@@ -1,24 +1,56 @@
 import React, { useState, useEffect, useRef, useCallback } from "react";
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
 import Api from "../api/Api";
 import "../styles/login.css";
 
-
 export default function TicketAllocation() {
 
-    const [data, setData] = useState([]);
-    
+    const [data, setData] = useState([]);    
+
+    const [open, setOpen] = React.useState(false);
+    const [options, setOptions] = React.useState([]);
+    const loading = open && options.length === 0;
     const [isSending, setIsSending] = useState(false)
     const isMounted = useRef(true)
 
-    const [ticketNumber, setTicketNumber] = useState(0);
-    const [person, setPerson] = useState([]);
-    const [name, setName] = useState("");
+    const [ticketNumberF, setTicketNumberF] = useState(null);
+    const [ticketNumberT, setTicketNumberT] = useState(null);
 
+    const [person, setPerson] = useState(null);
     const [eventID, setEventID] = useState(0);
 
     useEffect(() => {
-     return isMounted.current = false
-    },[]);
+      let active = true;
+  
+      if (!loading) {
+        return undefined;
+      }
+  
+      (async () => {
+   
+      const response = await Api.getRequest("person");
+      const persons = await response.json();
+
+        if (active) {
+          setOptions(persons);;
+        }
+      })();
+  
+      return () => {
+        active = false;
+      };
+    }, [loading]);
+
+
+    useEffect(() => {
+      if (!open) {
+        setOptions([]);
+      }
+    }, [open]);
+
 
     const allocateTicket = useCallback(async () => {
       // don't send again while we are sending
@@ -30,12 +62,13 @@ export default function TicketAllocation() {
       // send the actual request
 
       var x = {
-        "ticketNumber": parseInt(ticketNumber),
-        "person": eventID,
-        "event" : person.ID
+        "ticketNumberF": parseInt(ticketNumberF),
+        "ticketNumberT": parseInt(ticketNumberT),
+        "event": eventID,
+        "person" : person.id
       };
 
-      Api.postRequest("allocateTicket",x)
+      Api.postRequest("bulkAllocateTicket",x)
       .then(response => response.json())
       .then(data => {setData(data)});
 
@@ -44,57 +77,82 @@ export default function TicketAllocation() {
       if (isMounted.current) // only update if we are still mounted
         setIsSending(false)
 
-    }, [isSending, ticketNumber, eventID, person]); // update the callback if the state changes
+    }, [isSending, ticketNumberF, ticketNumberT, eventID, person]); // update the callback if the state changes
 
-    const findPerson = useCallback(async () => {
-      // don't send again while we are sending
-      if (isSending) return
+    const bob = () => {
+      console.log(person)
+      console.log(ticketNumberF)
+      console.log(ticketNumberT)
+    }
 
-      // update state
-      setIsSending(true)
-
-      // send the actual request
-
-      var x = {
-        "name" : name
-      };
-
-      Api.postRequest("personLikeName",x)
-      .then(response => response.json())
-      .then(data => {setPerson(data)});
-
-
-      // once the request is sent, update state again
-      if (isMounted.current) // only update if we are still mounted
-        setIsSending(false)
-
-    }, [isSending, name]); // update the callback if the state changes
-
-    
     return (
 
       <div className="App">
         <aside className="profile-card">
           <div className="profile-bio">
 
-      <input
-        type="text"
-        className="sign-up-input"
-        placeholder="Person name"
-        onChange={ e => setName(e.target.value)}
-      />
-      <input
-        type="text"
-        className="sign-up-input"
-        placeholder="ticket start number"
-        onChange={ e => setTicketNumber(e.target.value)}
+
+        <Autocomplete
+          id="asynchronous-demo"
+          style={{ width: 250 }}
+          open={open}
+          onOpen={() => { setOpen(true); }}
+          onClose={() => {setOpen(false); }}
+          getOptionSelected={(option, value) => option.name === value.name}
+          getOptionLabel={option => option.name +" " +option.surname }
+          options={options}
+          loading={loading}
+          value = {person}
+          onChange={(event, newValue) => { setPerson(newValue); }}
+          renderInput={params => (
+            <TextField
+              {...params}
+              label="Select Person"
+              variant="outlined"
+              InputProps={{
+                ...params.InputProps,
+                endAdornment: (
+                  <React.Fragment>
+                    {loading ? <CircularProgress color="inherit" size={20} /> : null}
+                    {params.InputProps.endAdornment}
+                  </React.Fragment>
+                ),
+              }}
+          />
+        )}
       />
 
-      <button className = "button" type="button" disabled={isSending} onClick={allocateTicket}> Allocate Ticket</button>
+
+      <TextField
+        id="filled-number"
+        label="ticket range from"
+        type="number"
+        onChange = {e => {setTicketNumberF(e.target.value)}}
+        InputLabelProps={{
+          shrink: true,
+        }}
+        variant="outlined"
+        
+      />
+
+      <TextField
+        id="filled-number"
+        label="ticket range to"
+        type="number"
+        onChange = {e => setTicketNumberT(e.target.value)}
+        InputLabelProps={{
+          shrink: true,
+        }}
+        variant="outlined"
+        
+      />
+
+      <button className = "button" type="button" disabled={isSending} onClick={bob}> Allocate Ticket</button> 
   
       </div>
       </aside>
       </div>
 
     );
+
 }
