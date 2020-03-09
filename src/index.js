00c343c089cc62a9bb5e176eb39a6194b2f7e3f8
RutendoN <RutendoN@Saratoga.local>
2020-03-09 14:47:36 +0200

    browser history

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Router, Route, Switch } from "react-router-dom";
-import createBrowserHistory from "history/createBrowserHistory";
+import {createBrowserHistory} from "history";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 
