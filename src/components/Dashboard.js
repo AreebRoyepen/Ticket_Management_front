d4d1184b231b2e8f68b98e09871d88505d491305
Areeb Royeppen <AreebR@Saratoga.local>
2020-03-10 11:14:52 +0200

    clean up

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -39,7 +39,7 @@ export default function Dashboard(){
     .then(data=> data.json())
     .then(data => setUnallocated(data))
 
-  },[setTickets])
+  },[setAllocated, setUnallocated])
 
     return (
         
