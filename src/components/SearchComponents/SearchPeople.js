3c6e038475433de2c9df6e2be81691d95b25d12e
rnyakuti <rnyakuti@student.wethinkcode.co.za>
2020-03-26 13:29:44 +0200

    popUp

--- src/components/SearchComponents/SearchPeople.js
+++ src/components/SearchComponents/SearchPeople.js
@@ -1,5 +1,8 @@
 import React,{Component, useState, useEffect} from 'react';
+import useModal from 'react-hooks-use-modal';
 import "../../styles/eventCard.css";
+import "../../styles/popUp.css";
+
 import { useHistory } from "react-router-dom";
 
 export default function SearchPeople (content) {
@@ -20,6 +23,36 @@ export default function SearchPeople (content) {
       console.log(items)
       setItems(items);
     }
+
+    
+const modalStyle = {
+  backgroundColor: 'white',
+  padding: '40px 45px',
+  marginLeft:"30px",
+  marginRight:"40px",
+  borderRadius: '10px',
+  marginTop:"140px",
+};
+
+
+const maskStyle = {
+  position: 'fixed',
+  top: 0,
+  left: 0,
+ 
+  bottom: 0,
+  right: 0,
+  backgroundColor: 'gray',
+  zIndex: 100000
+};
+
+
+    const [Modal, open, close, isOpen] = useModal('root', {
+      preventScroll: true,
+      backgroundColor: '#fff',
+      domNode:'cat'
+    });
+    
 
       return (
         <div>
@@ -43,7 +76,21 @@ export default function SearchPeople (content) {
                             </div>
                             <div className="card-sub-botton card-sub-show">
                             <input  onClick = {() => { console.log(x.id);  history.push("/PersonPage",{x:x, edit:true})}} type="submit" value="Edit" name="button"className="cardButtons  card-link u-float-right" id={JSON.stringify(x.active)}/>
-                            <input  type="submit" value="Delete" name="button"className="cardButtons  card-link u-float-right" id={JSON.stringify(x.active)}/>
+                           <cat>
+                           <button className = "cardButtons  card-link u-float-right"onClick={open}>Delete</button>
+                             <Modal style={maskStyle}>
+                               <div style={maskStyle}>
+                                 <div style={modalStyle}>
+                                 <h1>Delete</h1>
+                                 <span className="card-header">{x.id}</span>
+                                     <p>You are about to delete this person from your database. Click the delete button to confirm</p>
+                                     <input  type="submit" value="Delete" name="button"className="cardButtons" id={JSON.stringify(x.active)}/>
+                               <button className="cardButtons" onClick={close}>cancel</button>
+                                 </div>
+                                   
+                              </div>
+                             </Modal> 
+                             </cat>  
                             </div>
                         </div>
                     </div>
