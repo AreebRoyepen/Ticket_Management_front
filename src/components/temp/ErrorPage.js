01526e43a0ea6ad6bf10a4dd4f7717a240bf19bd
Areeb Royeppen <AreebR@Saratoga.local>
2020-04-01 15:41:33 +0200

    temp error page

---
+++ src/components/temp/ErrorPage.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './style.css'
+
+
+export const ErrorPage = () => (
+
+<div>
+
+<div id="notfound">
+    <div class="notfound">
+        <div class="notfound-404">
+            <h1>Oops!</h1>
+            <h2>Error while loading, please try again :)</h2>
+        </div>
+    </div>
+</div>
+
+</div>
+
+)
