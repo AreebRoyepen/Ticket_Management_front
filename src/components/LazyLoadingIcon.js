3aceec54890181812c015e29e325ee3824e60dfe
Areeb Royeppen <AreebR@Saratoga.local>
2020-04-02 19:10:30 +0200

    updated lazy loading icon

--- src/components/LazyLoadingIcon.js
+++ src/components/LazyLoadingIcon.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import "../styles/eventCard.css";
 
 export default function LoadingIcon (){
 
-    return <div>
-      <h5>
-        Loading ... 
-      </h5>
-    </div>
+    return(
+
+      <div className="container">
+      <div className="card">
+
+        <div className="card-body" >
+          <div className="card-header event-name">
+            <p>Loading ... </p>
+          </div>
+          <span className="card-header">
+          </span>
+          <div className="card-sub-botton card-sub-show">
+          </div>
+        </div>
+
+      </div>
+      </div>
+    );
   
 }
