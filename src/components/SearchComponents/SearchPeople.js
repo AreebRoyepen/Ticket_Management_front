5e068b9309a27fc2afcae1e7a75fb13802b8cbd2
Areeb Royeppen <AreebR@Saratoga.local>
2020-04-02 13:09:40 +0200

    authorization via roles for events

--- src/components/SearchComponents/SearchPeople.js
+++ src/components/SearchComponents/SearchPeople.js
@@ -1,4 +1,4 @@
-import React,{ useState } from 'react';
+import React,{ useState, useEffect } from 'react';
 import useModal from 'react-hooks-use-modal';
 import LazyLoad from 'react-lazyload';
 import Loading from '../LazyLoadingIcon';
@@ -10,85 +10,103 @@ import { useHistory } from "react-router-dom";
 export default function SearchPeople (content) {
 
 
-    const [initialItems, ] = useState(content.content);
-    const [items, setItems] = useState(content.content);
+  const [initialItems, ] = useState(content.content);
+  const [items, setItems] = useState(content.content);
+  let history = useHistory();
 
-    let history = useHistory();
+  const [user, setUser] = useState(null)
 
-    function filterList  (event){
-      let items = initialItems;
-      items = items.filter((item) => {
-        return JSON.stringify(item).toLowerCase().search(event.target.value.toLowerCase()) !== -1;
-      });
-      console.log(items)
-      setItems(items);
-    }
+  useEffect( () => {
+    setUser(JSON.parse(localStorage.user))  
+  },[setUser])
 
-    
-const modalStyle = {
-  backgroundColor: 'white',
-  padding: '40px 45px',
-  marginLeft:"30px",
-  marginRight:"40px",
-  borderRadius: '10px',
-  marginTop:"140px",
-};
-
-
-const maskStyle = {
-  position: 'fixed',
-  top: 0,
-  left: 0,
- 
-  bottom: 0,
-  right: 0,
-  backgroundColor: 'gray',
-  zIndex: 100000
-};
-
-
-    const [Modal, open, close, isOpen] = useModal('root', {
-      preventScroll: true,
-      backgroundColor: '#fff',
-      domNode:'cat'
+  function filterList  (event){
+    let items = initialItems;
+    items = items.filter((item) => {
+      return JSON.stringify(item).toLowerCase().search(event.target.value.toLowerCase()) !== -1;
     });
+    console.log(items)
+    setItems(items);
+  }
+
+    
+  const modalStyle = {
+    backgroundColor: 'white',
+    padding: '40px 45px',
+    marginLeft:"30px",
+    marginRight:"40px",
+    borderRadius: '10px',
+    marginTop:"140px",
+  };
+
+
+  const maskStyle = {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+  
+    bottom: 0,
+    right: 0,
+    backgroundColor: 'gray',
+    zIndex: 100000
+  };
+
+  const deleteButton = (x) =>{
+
+    if(user.role.id == 1)
+      return <input  type="submit" value="Delete" name="button"className="cardButtons card-link u-float-right" 
+      id={JSON.stringify(x)}/>
+  
+  }
+
+
+  const [Modal, open, close, isOpen] = useModal('root', {
+    preventScroll: true,
+    backgroundColor: '#fff',
+    domNode:'cat'
+  });
     
 
-      return (
-        <div>
-                <input className ="searchBoxStyle" type="search" placeholder="SEARCH" onChange={ e => filterList(e)}/>
-          <div>
+  return (
+    <div>
             
-            {items.map( x =>(
+    <input className ="searchBoxStyle" type="search" placeholder="SEARCH" onChange={ e => filterList(e)}/>
+    <div>
+        
+      {items.map( x =>(
 
-              <LazyLoad key = {x.id} placeholder = {<Loading />}>
-              <div key = {x.id}>
-                                    
-                <div className="container"> 
-
-                <div className="card">
-                    <div className="card-body" id ={JSON.stringify(x.active)}>
-                        <div className="card-header event-name">
-                            <p>{x.name + " " + x.surname}</p>
-                        </div>
-                        <span className="card-header">{x.number}<span className="card-header u-float-right"> {x.email} </span>
-                        </span>  
-                        
-
-                        <div className="card-sub-botton card-sub-show">
-                        <input  onClick = {() => { console.log(x.id);  history.push("/PersonPage",{x:x, edit:true})}} type="submit" value="Edit" name="button"className="cardButtons  card-link u-float-right" id={JSON.stringify(x.active)}/>
-                        <input  type="submit" value="Delete" name="button"className="cardButtons card-link u-float-right" id={JSON.stringify(x.active)}/>
-                        </div>
-                    </div>
+        <LazyLoad key = {x.id} placeholder = {<Loading />}>
+        <div key = {x.id}>                              
+          <div className="container"> 
+          <div className="card">
+              <div className="card-body" id ={JSON.stringify(x.active)}>
+                <div className="card-header event-name">
+                    <p>{x.name + " " + x.surname}</p>
                 </div>
-                </div>
-                </div>
+                <span className="card-header">{x.number}<span className="card-header u-float-right"> {x.email} </span>
+                </span>                 
 
-              </LazyLoad>
+                  <div className="card-sub-botton card-sub-show">
+                    <input  onClick = {() => { console.log(x.id);  history.push("/PersonPage",{x:x, edit:true})}} 
+                    type="submit" value="Edit" name="button"className="cardButtons  card-link u-float-right" id={JSON.stringify(x.active)}/>
+                    
+                    {user ?                    
+                    deleteButton(x.active)
+                    :
+                    <div/>
+                    }                  
+                  
+                  </div>
+              </div>
+          </div>
+          </div>
+          </div>
 
-               
-            ))}</div>
-        </div>
-      );
+        </LazyLoad>
+          
+      ))}
+  </div>
+  </div>
+  );
     
 };
