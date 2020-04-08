d33810a96dd747247eac4ba59f41634fe8d7d632
Areeb Royeppen <AreebR@Saratoga.local>
2020-04-08 17:07:05 +0200

    returned tickets repot

--- src/components/Reports.js
+++ src/components/Reports.js
@@ -179,6 +179,66 @@ export default function Reports() {
   
       }, [isSending, history, location, closeSnack]); // update the callback if the state changes
 
+    const returnedTickets = useCallback(async () => {
+        // don't send again while we are sending
+        if (isSending) return
+  
+        // update state
+        setIsSending(true)
+  
+        // send the actual request 
+        async function fetchData(){
+  
+          var time = 6000
+    
+            let t = await Api.reportRequest("returnedTickets",selectedOption) 
+            if(t.message === "success"){
+
+              const url = window.URL.createObjectURL(new Blob([t.data]));
+              const link = document.createElement('a');
+              link.href = url;
+              var date = new Date()
+              var filename = "REPORT Returned Ticket Totals for " + selectedOption + " Events " + date.toDateString() +".pdf"
+              link.setAttribute('download', filename);
+              document.body.appendChild(link);
+              link.click();
+
+              setOpenSnackbar({severity : "success", message : "Report to download shortly", open : true, time : time, closeType : closeSnack})
+
+  
+            }else if (t.message === "unauthorized"){
+              localStorage.clear();
+              history.push("/", {last : "/ReturnTickets", data : location.state})
+  
+            }else if(t.message === "error"){
+              time = 6000
+              setOpenSnackbar({severity : "error", message : "Unknown Error", open : true, time : time, closeType : closeSnack})
+  
+            }else if(t.message === "no connection"){
+              time = 6000
+              setOpenSnackbar({severity : "error", message : "Check your internet connection", open : true, time : time, closeType : closeSnack})
+  
+            }else if(t.message === "timeout"){
+              time = 6000
+              setOpenSnackbar({severity : "error", message : "Request timed out. Please Try Again", open : true, time : time, closeType : closeSnack})
+              
+            }else{
+              time = 6000
+              setOpenSnackbar({severity : "warning", message : t.message, open : true, time : time, closeType : closeSnack})
+  
+            }
+  
+                  
+  
+        }
+        
+        fetchData()
+  
+        // once the request is sent, update state again
+        if (isMounted.current) // only update if we are still mounted
+          setIsSending(false)
+  
+      }, [isSending, history, location, closeSnack]); // update the callback if the state changes
 
     return(
         
@@ -244,6 +304,13 @@ export default function Reports() {
           </button>
         </div>
 
+        <div >
+            <button
+              className="funButton headerButtons"
+              onClick = {() => returnedTickets()}>
+            returnedTickets Totals
+          </button>
+        </div>
 
             
 
